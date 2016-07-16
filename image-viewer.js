afcf6e16e4705c53d00bb407e59909e0571802fb
Hawken Rives <hawkrives@gmail.com>
2016-07-16 12:15:12 -0500

    make a nicer metadata viewer

--- image-viewer.js
+++ image-viewer.js
@@ -1,5 +1,20 @@
 'use strict'
 
+const map = require('lodash/map')
+const isArray = require('lodash/isArray')
+
+function PrettyJson({json}) {
+	json = json.toJSON()
+	return <dl>
+		{map(json, (v, k) => ([
+			<dt>{k}</dt>,
+			isArray(v)
+				? v.map(val => <dd>{val}</dd>)
+				: <dd>{v}</dd>
+		]))}
+	</dl>
+}
+
 function ImageViewer({image}) {
 	if (!image) {
 		return null
@@ -7,7 +22,7 @@ function ImageViewer({image}) {
 	return <div className='viewer'>
 		<img src={`file://${image.get('thumbPath')}`} />
 		<br/>
-		<div>{JSON.stringify(image.get('keywords'), null, 2)}</div>
+		<PrettyJson json={image.get('keywords')} />
 	</div>
 }
 module.exports = ImageViewer
